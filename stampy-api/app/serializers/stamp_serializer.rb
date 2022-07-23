5789619beceb7d336dac9f5359310853c6f44a7c
Aaron Amodt <aaron@amodttech.com>
2022-07-23 15:52:27 -0400

    image retreival working

--- stampy-api/app/serializers/stamp_serializer.rb
+++ stampy-api/app/serializers/stamp_serializer.rb
@@ -1,3 +1,11 @@
 class StampSerializer < ActiveModel::Serializer
-  attributes :id, :name, :description, :year, :country, :subject, :acquired, :cancelled
+  include Rails.application.routes.url_helpers
+
+  attributes :id, :name, :description, :year, :country, :subject, :acquired, :cancelled, :image
+
+
+  def image
+    return rails_blob_path(object.image, only_path: true) if object.image.attached?
+  end
+
 end
